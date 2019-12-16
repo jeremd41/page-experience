a2661a1b60ae98a656bc60996fb8862cb45871ff
Jeremy <jeremy.diard37@gmail.com>
2019-12-16 22:17:01 +0100

    first

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import "./styles.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hello !</h1>
+      <h2>I'm front dev' of page experience</h2>
     </div>
   );
 }

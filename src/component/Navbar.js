c592d18249cc9b99affada07b7a7836f7c846b49
Jeremy <jeremy.diard37@gmail.com>
2019-12-18 22:12:11 +0100

    UI free data

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -8,6 +8,34 @@ const Nav = styled.aside`
     height: 95vh;
     border: 1px solid rgba(0,0,0,.125);
 
+    ul{
+        border-top: 1px solid rgba(0,0,0,.125);
+        border-bottom: 1px solid rgba(0,0,0,.125);
+    }
+
+    i{
+        margin-right: 10px;
+    }
+
+    h1{
+        text-align: left;
+        margin: 10px;
+        text-transform: uppercase;
+        font-weight: bold;
+    }
+
+    p{
+        display: block;
+        margin: 0 auto;
+        width: 90%;
+    }
+
+    button{
+        display: block;
+        width: 90%;
+        margin: 10px auto;;
+    }
+
     li{
         text-align: left;
     }
@@ -18,15 +46,26 @@ class Navbar extends Component {
     render(){
         return(
             <Nav className="menu">
-                <ul className="menu-list">
-                <li>
-                    <Link>Home</Link>  
+                <h1 className="is-title is-1">Logo</h1>
+                <p className="control">
+                    <button className="button is-link">
+                    <i className="fas fa-user"/>   Profile
+                    </button>
+                </p>
+                <p className="control">
+                    <button className="button is-link">
+                    <i className="fas fa-plus"/>   Create
+                    </button>
+                </p>
+                <ul className="menu-list is-link">
+                <li className="is-link">
+                    <Link><i className="fas fa-home"/>Home</Link>  
                 </li>
                 <li>
-                    <Link>Journals</Link>  
+                    <Link><i className="fas fa-file-alt"/>Journals</Link>  
                 </li>
                 <li>
-                    <Link>Notifications</Link>  
+                    <Link><i className="fas fa-bell"/>Notifications</Link>  
                 </li>
                 </ul>
             </Nav>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,16 @@ const Wrapper = styled.div`{
         height:95vh;
         background: #fff;
         border: 1px solid rgba(0,0,0,.125);
+
+        h2{
+            text-align: left;
+            font-weight: bold;
+        }
+    }
+
+    .sendPost{
+        text-align: left;
+        margin-top: 25px;
     }
 
     .filcenter{
@@ -21,10 +31,30 @@ class Home extends Component {
         return(
             <Wrapper className="columns">
                 <div className="column filcenter">
-
+                <div class="field">
+                    <p className="control has-icons-left">
+                        <input className="input" type="text" placeholder="Search"/>
+                        <span className="icon is-small is-left">
+                        <i className="fas fa-search"></i>
+                        </span>
+                    </p>
                 </div>
-                <div className="column is-one-quarter filpost">
-                    <div></div>
+                </div>
+                <div className="column is-one-third filpost">
+                    <h2>What's happening</h2>
+                    <div className="sendPost">    
+                        <div className="field">
+                            <p className="label">Post :</p>
+                            <div className="control">
+                                <textarea className="textarea" placeholder="Send your post"></textarea>
+                            </div>
+                        </div>
+                        <div class="field is-grouped is-grouped-right">
+                            <div class="control">
+                                <button class="button is-link">Post</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </Wrapper>
         )
